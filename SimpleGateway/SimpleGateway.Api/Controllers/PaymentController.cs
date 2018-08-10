37f085d8385ebb421a3852165798721ec3d97150
Frederico Gomes <freddericogomes@hotmail.com>
2018-08-10 17:59:42 -0300

    Criação da camada de API

---
+++ SimpleGateway/SimpleGateway.Api/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleGateway.Api.Controllers
+{
+    [Route("/api/payment/transaction")]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public IActionResult Post([FromBody] object value)
+        {
+            return Created("", value);
+        }
+    }
+}
